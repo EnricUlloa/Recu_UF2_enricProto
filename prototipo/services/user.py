ed2740e0e31345baff2db77cd6e2d2286fc5516f
eulloa23 <eulloa23@ilg.cat>
2026-04-20 00:42:13 +0200

    exercici6

--- prototipo/services/user.py
+++ prototipo/services/user.py
@@ -57,4 +57,13 @@ def update_user(id, apellido, direccion):
     conn.close()
     return{"mensaje": "Usuario actualizado correctamente"}
 
+def delete_user(id):
+    conn = database.connection_db()
+    cursor = conn.cursor()
+    sql_delete = "DELETE FROM users WHERE id = %s"
+    cursor.execute(sql_delete, (id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return{"mensaje": "Usuario eliminado correctamente"}
 
